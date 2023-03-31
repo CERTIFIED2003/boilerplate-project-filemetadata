55347194d3a527d1d46412f95146d79ae378ba8f
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-03-31 20:08:37 +0530

    Completed File Metadata Microservice Project

--- index.js
+++ index.js
@@ -11,10 +11,23 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// --------------CHALLENGE--------------
+// Firstly install "multer". It is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files.
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+// Analyze Uploaded file API endpoint
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
